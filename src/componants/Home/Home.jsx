bfb7a99bf07816fe9d357be18bde674dbe8e8ac6
anissarkeraraf <anissarkeraraf@gmail.com>
2024-04-10 23:52:33 +0600

    added the featured card

--- src/componants/Home/Home.jsx
+++ src/componants/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Banner from "../Banner/Banner";
 import Card from "../Card/Card";
+import { useLoaderData } from "react-router-dom"
+import FeaturedCards from "../FeaturedCards/FeaturedCards";
 
 
 const Home = () => {
+
+    const home = useLoaderData();
+    console.log(home)
+
     return (
         <div>
             <Banner></Banner>
             <Card></Card>
+            <h2 className="text-4xl text-center mt-20 mb-10">Featured Properties</h2>
+            <div className="grid grid-cols-3 gap-10">
+
+                {
+                    home.map(aHome => <FeaturedCards key={aHome.id} home={aHome}></FeaturedCards>)
+                }
+            </div>
         </div>
     );
 };
